0b627a4b404ad48acf0a3bffa1c843c05a457192
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-01-19 14:46:20 -0300

    implementado pagina de erro
    Erro de pagina não encontrada

--- projetofilmes/src/routes.js
+++ projetofilmes/src/routes.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Header from './components/Header';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Erro from './pages/Erro';
 
 const Routes = () => {
     return(
@@ -13,6 +14,7 @@ const Routes = () => {
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/filme/:id" component={Filme}/>
                 <Route exact path="/favoritos" component={Favoritos}/>
+                <Route path="*" component={Erro}/>
             </Switch>
         </BrowserRouter>
     );
