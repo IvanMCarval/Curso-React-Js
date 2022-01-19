52ce8b2dac4ef12b4ecfad781460cc599f6feeae
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-01-19 10:37:13 -0300

    Implementado api de filmes e style na pagina home

--- projetofilmes/src/pages/Home/index.js
+++ projetofilmes/src/pages/Home/index.js
@@ -1,9 +1,34 @@
 //https://sujeitoprogramador.com/r-api/?api=filmes/
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import './home.css'
 
 export default function Home() {
-    return (
-      <div>
-        <h1>Home</h1>
+  const [filmes, setFilmes] =  useState([]);
+
+  useEffect(()=>{
+    async function loadFilmes(){
+      const response = await api.get('r-api/?api=filmes/')
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="lista-filme">
+        {filmes.map((filme)=>{
+          return(
+            <article key={filme.id}>
+              <strong>{filme.nome}</strong>
+              <img src={filme.foto} alt={filme.nome}/>
+              <Link to="/">Acessar</Link>
+            </article>
+          );
+        })}
       </div>
-    );
+    </div>
+  );
   }
