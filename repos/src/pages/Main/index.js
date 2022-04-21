807db83aa400bda8495a49d58c985e21c1ac850c
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-04-21 14:46:31 -0300

    implementado botão de deletar repositorio

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -2,8 +2,8 @@ import { useCallback, useState } from 'react';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'
 
 export default function Main() {
     const [newRepo, setNewRepo] = useState('');
@@ -42,6 +42,11 @@ export default function Main() {
         
     }, [newRepo, repositorios]);
 
+    const handleDelete = useCallback((repo) => {
+        const find = repositorios.filter(r => r.name !== repo);
+        setRepositorios(find);
+    }, [repositorios]);
+
     return(
         <Container>
             <h1>
@@ -60,6 +65,22 @@ export default function Main() {
                     )}
                 </SubmitButton>
             </Form>
+
+            <List>
+                {repositorios.map(repo => (
+                    <li key={repo.name}>
+                        <span>
+                            <DeleteButton onClick={() => handleDelete(repo.name)}>
+                                <FaTrash size={14}/>
+                            </DeleteButton>
+                            {repo.name}
+                        </span>
+                        <a href=''>
+                            <FaBars size={20}/>
+                        </a>
+                    </li>
+                ))}
+            </List>
         </Container>
     )
 }
--- repos/src/pages/Main/styles.js
+++ repos/src/pages/Main/styles.js
@@ -34,6 +34,39 @@ export const Form = styled.form`
     }
 `;
 
+export const List = styled.ul`
+    list-style: none;
+    margin-top: 20px;
+
+    li{
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        
+        & + li{
+            border-top: 1px solid #111111;
+        }
+
+        a{
+            color: #0D2636;
+            text-decoration: none;
+        }
+    }
+`;
+
+export const DeleteButton = styled.button.attrs({
+    type: 'button'
+})`
+    padding: 8px 7px;
+    background: transparent;
+    color: #0D2636;
+    border: 0;
+    outline: 0;
+    border-radius: 4px;
+`;
+
 //Animação do botão
 const animate = keyframes`
     from{
