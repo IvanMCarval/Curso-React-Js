16459381bf4abb6cae84bcbc61c2c1614a5bf4e0
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-05-11 14:39:36 -0300

    configurado redux

--- trips/src/App.js
+++ trips/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './Store';
 import Routes from './Routes/routes';
 import Header from './Components/Header';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header/>
+        <Routes/>
+      </BrowserRouter>
+    </Provider>
   );
 }
